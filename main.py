89768e9ea2a652d1b2c983cc8d56db0654be7515
gloordaria <55581650+gloordaria@users.noreply.github.com>
2019-10-28 15:09:24 +0100

    Einheitliche Struktur für HTML-Seiten

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-test
+from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import request
+from flask import url_for
+
+app = Flask("Lernaufwandrechner")
+
+@app.route("/")
+def startseite():
+    return url_for("startseite.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+
+
+
