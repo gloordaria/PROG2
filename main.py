ee76cface353c62278bcb1156817acaf4f99618e
gloordaria <55581650+gloordaria@users.noreply.github.com>
2019-11-08 16:15:14 +0100

    Formulardaten in main.py eingespeist

--- main.py
+++ main.py
@@ -13,15 +13,50 @@ def startseite():
 
 @app.route("/modulerfassen")
 def modulerfassen():
+
+    if request.method == 'POST':
+        modulname = request.form['modulname']
+        return modulname
+
+    if request.method == 'POST':
+        credits = request.form['credits']
+        return credits
+
+    if request.method == 'POST':
+        semester = request.form['semester']
+        return semester
+
+    if request.method == 'POST':
+        vorlesungen = request.form['vorlesungen']
+        return vorlesungen
+
+
     return render_template("modulerfassen.html")
+
+
 
 @app.route("/moduluebersicht")
 def moduluebersicht():
     return render_template("moduluebersicht.html")
 
+
+
+
 @app.route("/lernzeit")
 def lernzeit():
+
+    if request.method == 'POST':
+        datum = request.form['datum']
+        return datum
+
+    if request.method == 'POST':
+        lernzeit = request.form['lernzeit']
+        return lernzeit
+
     return render_template("lernzeit.html")
+
+
+
 
 @app.route("/lernzeitdetail")
 def lernzeitdetail():
